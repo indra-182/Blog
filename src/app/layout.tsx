5121df7f80a456925c5b910cc0b8336bad21f552
indra-182 <mahadiindra2@gmail.com>
2026-07-26 16:41:14 +0700

    default theme light, disable system

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-dvh flex flex-col">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
           <div className="flex flex-col flex-1 w-full max-w-4xl mx-auto px-4 pt-24 pb-6">
             <Navbar />
             <main className="flex-1">{children}</main>
